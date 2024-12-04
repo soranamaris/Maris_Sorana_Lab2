3ce8f2d7e5459ed0b5c520e44903d23ad813bcda
Sorana Maris <sorana.maris@stud.ubbcluj.ro>
2024-12-04 18:47:15 +0200

    Lab10

---
+++ Controllers/CustomersGrpcController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Grpc.Net.Client;
+using GrpcCustomersService;
+using Maris_Sorana_Lab2.Models;
+namespace Maris_Sorana_Lab2.Controllers
+{
+    public class CustomersGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public CustomersGrpcController()
+        {
+            channel = GrpcChannel.ForAddress("http://localhost:5194");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(GrpcCustomersService.Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new
+                CustomerService.CustomerServiceClient(channel);
+                var createdCustomer = client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+    }
+}
